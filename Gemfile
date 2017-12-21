3becc0e4a5ffab4796178c0777bf1fa2e4facadf
Irene <musiclover.0122@gmail.com>
2017-12-21 09:16:42 -0500

    add .env to gitignore file

--- Gemfile
+++ Gemfile
@@ -55,4 +55,4 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'bootstrap', '~> 4.0.0.beta2.1'
 gem 'jquery-rails'
-gem 'dotenv-rails'
+gem 'dotenv-rails', :groups => [:development, :test]
--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -5,7 +5,7 @@ class MapsController < ApplicationController
   def show
   	@location = params[:location]
   	@zip = params[:zip]
-  	@map = "https://www.google.com/maps/embed/v1/place?key=AIzaSyBtpU9M3Rp9IMA7GxuBX6ALxVsPLOfmVEc
+  	@map = "https://www.google.com/maps/embed/v1/place?key=DEMO_KEY_GOOGLE
     &q=Best+{@location}+In+#{@zip}+NYC"
   end
 end
